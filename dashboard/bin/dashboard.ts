7de8040aad48ba6b11df34e350c106896d265fce
Don Simpson <donsi@amazon.com>
2024-01-12 15:59:09 +0000

    cleanup

--- dashboard/bin/dashboard.ts
+++ dashboard/bin/dashboard.ts
@@ -16,9 +16,5 @@ new DashboardStack(app, "DashboardStack", {
     region: process.env.CDK_DEFAULT_REGION,
   },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });

d763c90861ade15771e2d65abe64b881543b0733
Don Simpson <donsi@amazon.com>
2024-01-12 19:00:18 +0000

    Copyright and formatting

--- dashboard/bin/dashboard.ts
+++ dashboard/bin/dashboard.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 import "source-map-support/register";
 import { App } from "aws-cdk-lib";
 import { DashboardStack } from "../lib/dashboard-stack";
--- dashboard/test/dashboard.test.ts
+++ dashboard/test/dashboard.test.ts
@@ -1,208 +1,185 @@
-import { App, Duration, RemovalPolicy } from 'aws-cdk-lib';
-import { Match, Template } from 'aws-cdk-lib/assertions';
-import * as Dashboard from '../lib/dashboard-stack';
-import { Key, KeySpec, KeyUsage } from 'aws-cdk-lib/aws-kms';
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { App, Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import * as Dashboard from "../lib/dashboard-stack";
+import { Key, KeySpec, KeyUsage } from "aws-cdk-lib/aws-kms";
 
 const app = new App();
-const stack = new Dashboard.DashboardStack(app, 'DashboardStack');
+const stack = new Dashboard.DashboardStack(app, "DashboardStack");
 const template = Template.fromStack(stack);
 
-test('KMS Key Test', () => {
-
-  template.hasResourceProperties('AWS::KMS::Key', {
-      KeySpec: KeySpec.SYMMETRIC_DEFAULT,
-      KeyUsage: KeyUsage.ENCRYPT_DECRYPT
+test("KMS Key Test", () => {
+  template.hasResourceProperties("AWS::KMS::Key", {
+    KeySpec: KeySpec.SYMMETRIC_DEFAULT,
+    KeyUsage: KeyUsage.ENCRYPT_DECRYPT,
   });
-  
 });
 
-test('S3 Bucket w/ KMS Key Encryption Test', () => {
-
-  template.hasResourceProperties('AWS::S3::Bucket',
+test("S3 Bucket w/ KMS Key Encryption Test", () => {
+  template.hasResourceProperties(
+    "AWS::S3::Bucket",
     Match.objectLike({
-        'BucketEncryption': 
-        {
-            'ServerSideEncryptionConfiguration': [
-                {
-                    'ServerSideEncryptionByDefault': {
-                        'SSEAlgorithm': 'aws:kms'
-                    }
-                }
-            ]
-        }
+      BucketEncryption: {
+        ServerSideEncryptionConfiguration: [
+          {
+            ServerSideEncryptionByDefault: {
+              SSEAlgorithm: "aws:kms",
+            },
+          },
+        ],
+      },
     })
   );
-  
 });
 
-test('Conformance Pack Compliance Summary DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'conformance-pack-compliance-summary'
-      })
+test("Conformance Pack Compliance Summary DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
+    Match.objectLike({
+      TableName: "conformance-pack-compliance-summary",
+    })
   );
-  
 });
 
-test('Conformance Pack Compliance Detail DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'conformance-pack-compliance-detail'
-      })
+test("Conformance Pack Compliance Detail DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
+    Match.objectLike({
+      TableName: "conformance-pack-compliance-detail",
+    })
   );
-  
 });
 
-test('Conformance Pack Compliance Playbook DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'conformance-pack-compliance-playbook'
-      })
+test("Conformance Pack Compliance Playbook DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
+    Match.objectLike({
+      TableName: "conformance-pack-compliance-playbook",
+    })
   );
-  
 });
 
-test('Conformance Pack Compliance Remediation DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'conformance-pack-compliance-remediation'
-      })
+test("Conformance Pack Compliance Remediation DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
+    Match.objectLike({
+      TableName: "conformance-pack-compliance-remediation",
+    })
   );
-  
 });
 
-test('Conformance Pack Compliance Scheduled Event Test', () => {
-
-  template.hasResourceProperties('AWS::Events::Rule', {
-      ScheduleExpression: 'cron(0/5 * * * ? *)'
+test("Conformance Pack Compliance Scheduled Event Test", () => {
+  template.hasResourceProperties("AWS::Events::Rule", {
+    ScheduleExpression: "cron(0/5 * * * ? *)",
   });
-  
 });
 
-test('Conformance Pack Compliance Processor Lambda Function Test', () => {
-
-  template.hasResourceProperties('AWS::Lambda::Function', 
+test("Conformance Pack Compliance Processor Lambda Function Test", () => {
+  template.hasResourceProperties(
+    "AWS::Lambda::Function",
     Match.objectLike({
-        'FunctionName': Match.stringLikeRegexp('-conformance-pack-compliance-processor')
+      FunctionName: Match.stringLikeRegexp(
+        "-conformance-pack-compliance-processor"
+      ),
     })
   );
-  
 });
 
-test('Questionnaire Answers DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'questionnaire-answers'
-      })
-  );
-  
-});
-
-test('Questionnaire Answers Risks DynamoDB Table Test', () => {
-
-  template.hasResourceProperties('AWS::DynamoDB::Table', 
-      Match.objectLike({
-        'TableName': 'questionnaire-answers-risks'
-      })
-  );
-  
-});
-  
-test('Well-Architected Triggered Event Test', () => {
-
-  template.hasResourceProperties('AWS::Events::Rule',
+test("Questionnaire Answers DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
     Match.objectLike({
-      'EventPattern': {
-        'source': [
-          'aws.wellarchitected'
-        ]
-      }
+      TableName: "questionnaire-answers",
     })
   );
-  
 });
 
-test('Well-Architected Event Processor Lambda Function Test', () => {
-
-  template.hasResourceProperties('AWS::Lambda::Function', 
+test("Questionnaire Answers Risks DynamoDB Table Test", () => {
+  template.hasResourceProperties(
+    "AWS::DynamoDB::Table",
     Match.objectLike({
-        'FunctionName': Match.stringLikeRegexp('-well-architected-event-processor')
+      TableName: "questionnaire-answers-risks",
     })
   );
-  
 });
 
-test('Athena Data Catalog Test', () => {
+test("Well-Architected Triggered Event Test", () => {
+  template.hasResourceProperties(
+    "AWS::Events::Rule",
+    Match.objectLike({
+      EventPattern: {
+        source: ["aws.wellarchitected"],
+      },
+    })
+  );
+});
 
-  template.hasResourceProperties('AWS::Athena::DataCatalog', {
-      Type: 'LAMBDA'
+test("Well-Architected Event Processor Lambda Function Test", () => {
+  template.hasResourceProperties(
+    "AWS::Lambda::Function",
+    Match.objectLike({
+      FunctionName: Match.stringLikeRegexp("-well-architected-event-processor"),
+    })
+  );
+});
+
+test("Athena Data Catalog Test", () => {
+  template.hasResourceProperties("AWS::Athena::DataCatalog", {
+    Type: "LAMBDA",
   });
-  
 });
 
-test('QuickSight Data Source Test', () => {
-
-  template.hasResourceProperties('AWS::QuickSight::DataSource', 
+test("QuickSight Data Source Test", () => {
+  template.hasResourceProperties(
+    "AWS::QuickSight::DataSource",
     Match.objectLike({
-        'DataSourceId': Match.stringLikeRegexp('-dynamodb')
+      DataSourceId: Match.stringLikeRegexp("-dynamodb"),
     })
   );
-  
 });
 
-test('QuickSight Conformance Pack Compliance Summary Dataset Test', () => {
-
-  template.hasResourceProperties('AWS::QuickSight::DataSet', 
+test("QuickSight Conformance Pack Compliance Summary Dataset Test", () => {
+  template.hasResourceProperties(
+    "AWS::QuickSight::DataSet",
     Match.objectLike({
-        'DataSetId': 'conformance-pack-compliance-summary'
+      DataSetId: "conformance-pack-compliance-summary",
     })
   );
-  
 });
 
-test('QuickSight Conformance Pack Compliance Remediation Dataset Test', () => {
-
-  template.hasResourceProperties('AWS::QuickSight::DataSet', 
+test("QuickSight Conformance Pack Compliance Remediation Dataset Test", () => {
+  template.hasResourceProperties(
+    "AWS::QuickSight::DataSet",
     Match.objectLike({
-        'DataSetId': 'conformance-pack-compliance-remediation'
+      DataSetId: "conformance-pack-compliance-remediation",
     })
   );
-  
 });
 
-test('QuickSight Conformance Pack Compliance Detail Dataset Test', () => {
-
-  template.hasResourceProperties('AWS::QuickSight::DataSet', 
+test("QuickSight Conformance Pack Compliance Detail Dataset Test", () => {
+  template.hasResourceProperties(
+    "AWS::QuickSight::DataSet",
     Match.objectLike({
-        'DataSetId': 'conformance-pack-compliance-detail'
+      DataSetId: "conformance-pack-compliance-detail",
     })
   );
-  
 });
 
-test('QuickSight Questionnaire Answers Risks Dataset Test', () => {
-
-  template.hasResourceProperties('AWS::QuickSight::DataSet', 
+test("QuickSight Questionnaire Answers Risks Dataset Test", () => {
+  template.hasResourceProperties(
+    "AWS::QuickSight::DataSet",
     Match.objectLike({
-        'DataSetId': 'questionnaire-answers-risks'
+      DataSetId: "questionnaire-answers-risks",
     })
   );
-  
 });
 
-test('QuickSight Analysis Test', () => {
-
-  template.resourceCountIs('AWS::QuickSight::Analysis',1);
-  
+test("QuickSight Analysis Test", () => {
+  template.resourceCountIs("AWS::QuickSight::Analysis", 1);
 });
 
-test('QuickSight Dashboard Test', () => {
-
-  template.resourceCountIs('AWS::QuickSight::Dashboard',1);
-  
+test("QuickSight Dashboard Test", () => {
+  template.resourceCountIs("AWS::QuickSight::Dashboard", 1);
 });
